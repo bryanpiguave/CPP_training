560c479ce5e7e1cf82b274a0dcae76d43b9b4f70
bryanpiguave <bpiguave@espol.edu.ec>
2023-10-12 10:42:42 -0500

    adding classes exercise

--- classes.cpp
+++ classes.cpp
@@ -8,14 +8,35 @@ public:  //these attribute of class are available in other functions and classes
     int age = 5;
     int size = 5;
     bool healthy = true;
+
+
+    void output(); //Declaring output
+    void set_name(string new_name);
+    string get_name();
+private:
+    bool good_dog = true;
+
 };
+
+void Dog::set_name(string new_name){
+    name = new_name;
+}
+string Dog::get_name(){
+    return  name;
+}
+
+void Dog::output(){   //indicates function output is member of Dog class
+    cout << "Dog name is: "<<name<<endl;    
+    cout << "Dog gender is: "<<gender<<endl;   
+    cout << "Dog age is: "<<age<<endl;
+    cout << "Dog size is: "<<size<<endl;
+    cout << "Is dog healthy: " <<healthy<<endl;
+}
 
 
 int main() {
   Dog dogObj;     //making object of Dog class
-  cout << "Dog name is: "<<dogObj.name<<endl;     //by using . operator we can access the member of class
-  cout << "Dog gender is: "<<dogObj.gender<<endl;    //accessing the public members of class Dog in main()
-  cout << "Dog age is: "<<dogObj.age<<endl;
-  cout << "Dog size is: "<<dogObj.size<<endl;
-  cout << "Is dog healthy: " <<dogObj.healthy<<endl;
+  dogObj.output();
+  dogObj.set_name("Hola");
+  std::cout << dogObj.get_name() << std::endl;
 }
